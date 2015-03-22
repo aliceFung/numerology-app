e12cfb6d2ec7fc3e548ca6e33e07765d4ab18241
--replace-all <krystallattice@gmail.com>
2015-03-22 22:23:23 +0000

    person show page

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+set :bind, '0.0.0.0'
+
+get '/people/:id' do
+    "This is a person show page"
+#    @person = Person.find(params[:id])
+#    @first_name = @person[:first_name]
+#    @last_name = @person[:last_name]
+#    @birthdate = @person[:birthdate]
+#    birthdate_string = @person.birthdate.strftime("%m%d%Y")
+#    birth_path_num = Person.birth_path_num(birthdate_string)
+#    @message = Person.numerology_msg(birth_path_num)
+#    erb :"/people/show"
+end
