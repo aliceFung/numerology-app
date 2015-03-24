0a1ab19837442aaca5cd04e72e0ff94f8f17e585
--replace-all <krystallattice@gmail.com>
2015-03-24 17:34:36 +0000

    new person pg

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,28 @@
 require 'sinatra'
 set :bind, '0.0.0.0'
 
+get '/people' do
+    @people = Person.all
+    erb :"/people/index"
+end
+
+get '/people/new' do
+    @person = Person.new
+    erb :"/people/new"
+end
+
+#post '/people' do
+#    if Person.valid_birthdate(params[:birthdate])
+#        birthdate = params[:birthdate]
+#    else
+#        birthdate = Date.strftime(params[:birthdate], "%m%d%Y")
+#    end
+    
+#    @person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+#    @id = person.id
+#    redirect "/people/#{@person.id}"
+#end
+
 get '/people/:id' do
 #    "This is a person show page"
     @person = Person.find(params[:id])
@@ -11,10 +33,4 @@ get '/people/:id' do
     birth_path_num = Person.get_birth_path_num(birthdate_string)
     @message = Person.numerology_msg(birth_path_num)
     erb :"/people/show"
-end
-
-get '/people' do
-    @people = Person.all
-    erb :"/people/index"
-end
-    
+end   
