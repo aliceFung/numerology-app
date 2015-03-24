ca2897287059339761634c8fbe0bdb000c36b89a
--replace-all <krystallattice@gmail.com>
2015-03-24 21:11:42 +0000

    edit a person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -16,8 +16,7 @@ post '/people' do
     birthdate = params[:birthdate]
   else
       birthdate = DateTime.strptime(params[:birthdate], "%m%d%Y")
-  end
-    
+  end    
     person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
     redirect "/people/#{person.id}"
 end
@@ -29,3 +28,17 @@ get '/people/:id' do
     @message = Person.numerology_msg(birth_path_num)
     erb :"/people/show"
 end   
+
+get '/people/:id/edit' do
+    @person = Person.find(params[:id])
+    erb :"/people/edit"
+end
+
+put '/people/:id' do
+    person = Person.find(params[:id])
+    person.first_name = params[:first_name]
+    person.last_name = params[:last_name]
+    person.birthdate = params[:birthdate]
+    person.save
+    redirect "/people/#{person.id}"
+end
