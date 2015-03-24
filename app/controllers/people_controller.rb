05444e962d10479743900bb2110d13d19a51f3d9
--replace-all <krystallattice@gmail.com>
2015-03-24 20:00:19 +0000

    testing input new person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -12,13 +12,17 @@ get '/people/new' do
 end
 
 post '/people' do
-    if Person.valid_birthdate(params[:birthdate])
-        birthdate = params[:birthdate]
-    elsif birthdate.nil?
+  if params[:birthdate].include?("-")
+    birthdate = params[:birthdate]
+  else
+      birthdate = DateTime.strptime(params[:birthdate], "%m%d%Y")
+  end
+    #step 2 check, error for birthdate = nil    Is it an input form error? Dashes are ok, 12/12/2001 is not, nor 02feb12
+  if birthdate.nil?
         birthdate=Time.now
-    else
-        birthdate = Date.strptime(params[:birthdate], "%m%d%Y")
-    end
+  else
+      birthdate
+  end
     
     person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
 #    @id = person.id
@@ -30,9 +34,9 @@ get '/people/:id' do
     @person = Person.find(params[:id])
 #    @first_name = @person[:first_name]
 #    @last_name = @person[:last_name]
-#    @birthdate = @person[:birthdate]
-    birthdate_string = @person.birthdate.strftime("%m%d%Y")
-    birth_path_num = Person.get_birth_path_num(birthdate_string)
-    @message = Person.numerology_msg(birth_path_num)
+    @message = @person[:birthdate]
+#    birthdate_string = @person.birthdate.strftime("%m%d%Y")
+#    birth_path_num = Person.get_birth_path_num(birthdate_string)
+#    @message = Person.numerology_msg(birth_path_num)
     erb :"/people/show"
 end   
