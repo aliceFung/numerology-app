aafbaed68e0b99ae62bad3ad553bab0f67303415
--replace-all <krystallattice@gmail.com>
2015-03-24 20:16:34 +0000

    create people fixed

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -17,26 +17,15 @@ post '/people' do
   else
       birthdate = DateTime.strptime(params[:birthdate], "%m%d%Y")
   end
-    #step 2 check, error for birthdate = nil    Is it an input form error? Dashes are ok, 12/12/2001 is not, nor 02feb12
-  if birthdate.nil?
-        birthdate=Time.now
-  else
-      birthdate
-  end
     
     person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
-#    @id = person.id
     redirect "/people/#{person.id}"
 end
 
 get '/people/:id' do
-#    "This is a person show page"
     @person = Person.find(params[:id])
-#    @first_name = @person[:first_name]
-#    @last_name = @person[:last_name]
-    @message = @person[:birthdate]
-#    birthdate_string = @person.birthdate.strftime("%m%d%Y")
-#    birth_path_num = Person.get_birth_path_num(birthdate_string)
-#    @message = Person.numerology_msg(birth_path_num)
+    birthdate_string = @person.birthdate.strftime("%m%d%Y")
+    birth_path_num = Person.get_birth_path_num(birthdate_string)
+    @message = Person.numerology_msg(birth_path_num)
     erb :"/people/show"
 end   
