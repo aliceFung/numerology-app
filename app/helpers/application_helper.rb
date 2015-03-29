5fff44b41655cb0de8677fcfcbf73561a059568f
--replace-all <krystallattice@gmail.com>
2015-03-29 00:54:17 +0000

    method helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+    def people_form_method(person)
+        if person.new_record?
+            @method = "post"
+        else
+            @method = "put"
+        end
+        @method
+    end
+    
+#    def people_form_action(person)
+#        if person.new_record?
+#            @action = "<%= "/people/#{@person.id}" %>"
+#        else
+#            @action = "/people"
+#    end
 end
