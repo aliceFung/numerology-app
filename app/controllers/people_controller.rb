3672c27a1bc29ff1967bcd0d69e55267f5b907f0
--replace-all <krystallattice@gmail.com>
2015-03-29 14:33:02 +0000

    added new person errors

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -14,17 +14,24 @@ get '/people/new' do
 end
 
 post '/people' do
-  if params[:birthdate].include?("-")
-    birthdate = params[:birthdate]
-  else
-      birthdate = DateTime.strptime(params[:birthdate], "%m%d%Y")
-  end    
-    @person = Person.new(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
+    @person = Person.new(first_name: params[:first_name], last_name: params[:last_name], birthdate: params[:birthdate])
     if @person.valid?
+        if params[:birthdate].include?("-")
+            birthdate = params[:birthdate]
+#        elsif !Person.valid_birthdate(birthdate.gsub("-",""))
+#            @errors = "You should enter a valid birthdate in the form of mmddyyyy."
+#            erb :"/people/new"
+        else
+            birthdate = DateTime.strptime(params[:birthdate], "%m%d%Y") #careful: interprets input as DD-MM-YY; if entered as MM-DD-YY && DD >12, birthdate error.
+        end
         @person.save
-    redirect "/people/#{person.id}"
+        redirect "/people/#{person.id}"
     else
-        @error="The data you entered isn't valid"
+#        @error = "The data you entered isn't valid"
+        @errors = ''
+        @person.errors.full_messages.each do |message|
+            @errors = "#{@errors} #{message}."
+        end
         erb :"/people/new"
     end
 end
